8138838db4f9f8455ad8f07a421f77f6c0c52611
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-07-18 17:30:46 -0300

    fix: adjust home icon

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -28,7 +28,7 @@ export function AppRoutes() {
         name="products"
         component={Home}
         options={{
-          tabBarIcon: ({ color }) => <Ionicons name="md-home-outline" color={color} size={sizes[6]} />
+          tabBarIcon: ({ color }) => <Ionicons name="home-outline" color={color} size={sizes[6]} />
         }}
       />
 
