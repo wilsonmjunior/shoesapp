6b658ecb9d6381cf9662d260f08b6181c6cbf00f
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-07-27 22:44:01 -0300

    feat: config deep linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,25 @@
 import { useEffect, useState } from 'react';
 import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, LinkingOptions, NavigationContainer } from '@react-navigation/native';
 import { NotificationWillDisplayEvent, OneSignal, OSNotification } from 'react-native-onesignal';
 
 import { AppRoutes } from './app.routes';
 import { Notification } from '../components/Notification';
+
+const linking = {
+  prefixes: ['igniteshoesapp://', 'com.juniors.igniteshoesapp://'],
+  config: {
+    screens: {
+      details: {
+        path: '/details/:productId',
+        parse: {
+          productId: (productId: string) => productId
+        }
+      }
+    }
+  }
+} as LinkingOptions<ReactNavigation.RootParamList> | undefined
+
 
 export function Routes() {
   const [notification, setNotification] = useState<OSNotification>();
@@ -30,7 +45,7 @@ export function Routes() {
   }, [])
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       { notification?.title ? <Notification data={notification} onClose={() => setNotification(undefined)} /> : null }
